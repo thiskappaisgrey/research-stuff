1ed1878d5f701946558252384275217316ea59b6
ThisKappaIsGrey <thanatechaumnuaiwit@gmail.com>
2023-10-27 18:09:36 -0700

    Did some initial experiments

--- flake.nix
+++ flake.nix
@@ -1,22 +1,36 @@
 {
   inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
-
-  outputs = {self, nixpkgs, clash}:
+  inputs.lakeroad-yosys = {
+      type = "github";
+      owner = "uwsampl";
+      repo = "yosys";
+      ref = "gussmith23/lakeroad-backend";
+      flake = false;
+  };
+  outputs = {self, nixpkgs, lakeroad-yosys }:
     let 
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
+      yosys1 = pkgs.yosys.overrideAttrs (finalAttrs: previousAttrs: {
+        pname = "lakeroad-" + previousAttrs.pname;
+        src = lakeroad-yosys;
+        buildInputs = previousAttrs.buildInputs ++ [ pkgs.boost ];
+      });
     in
       {
         devShells."${system}".default = pkgs.mkShell {
+          inputsFrom = [ yosys1 ];
+
           buildInputs = with pkgs; [
             cargo
             rustc
             verilog
             racket
-            # z3
             yosys
          ];
-        }; 
+        };
+        packages."${system}".lakeroad-yosys = yosys1;
+
       };
     # organist.flake.outputsFromNickel ./. inputs {};}
 }
